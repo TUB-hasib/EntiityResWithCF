fdf9752fd408e83b7e4be31f49dc7f75a90d13e5
TUB-Hasib <h.alam@tu-berlin.de>
2025-02-12 13:47:24 +0100

    adding git ignore

--- airflow/dags/er_dag_test.py
+++ airflow/dags/er_dag_test.py
@@ -1,98 +1,57 @@
 from airflow import DAG
-from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
-
-from airflow.models import Connection
-from airflow.settings import Session
+import requests
 
 
 
-# Function to add HTTP connections dynamically
-def add_connection(conn_id, host, port):
-    session = Session()
-    existing_conn = session.query(Connection).filter(Connection.conn_id == conn_id).first()
-    
-    if not existing_conn:
-        new_conn = Connection(
-            conn_id=conn_id,
-            conn_type="http",
-            host=host,
-            port=port
-        )
-        session.add(new_conn)
-        session.commit()
-        print(f"Added connection: {conn_id}")
-    else:
-        print(f"Connection {conn_id} already exists")
+def f_calculate_quality_measures():
+    try:
+        url = 'http://host.docker.internal:8084/func_calculate_quality_measures'
+        # response = requests.get('http://localhost:8084/func_calculate_quality_measures')
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()  # Raises an error for 4xx/5xx responses
+        print("Response:", response.text)
+    except requests.exceptions.RequestException as e:
+        print(f"Error calling API: {e}")
+        raise  # Ensure Airflow marks the task as failed
+
+    #     return {"status": "error", "message": f"Failed to call func_calculate_quality_measures : {response.text}"}, response.status_code
+
+# Define the Python function
+def func_test():
+    print("Hello from PythonOperator!")
 
 
-# Add connections for local functions
-add_connection("local_func_calculate_quality_measures", "http://127.0.0.1", 8084)
-# add_connection("local_func_er_clustering", "http://127.0.0.1", 8085)  
-  
-
+# Default arguments
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'email_on_failure': False,
+    
     'email_on_retry': False,
     'retries': 0,
 }
 
+# Define the DAG
 with DAG(
-    'a_er_dag_test',
+    'a_er_calculate_quality_measures_dag',
     default_args=default_args,
-    description='test dag for entity resolution',
+    description='A simple PythonOperator DAG to test ER',
     schedule_interval=None,
     start_date=days_ago(1),
-    tags=['entity resolution'],
+    tags=['entity resolution', 'quality measures'],
+
 ) as dag:
 
-    # func_file_extraction = SimpleHttpOperator(
-    #     task_id='func_file_extraction',
-    #     http_conn_id='http_default',
-    #     endpoint='func_file_extraction',
-    #     method='GET',
-    #     response_check=lambda response: response.status_code == 200,
-    #     dag=dag,
-    # )
-
-    # func_er_with_blocking = SimpleHttpOperator(
-    #     task_id='func_er_with_blocking',
-    #     http_conn_id='http_default',
-    #     endpoint='func_er_with_blocking',
-    #     method='GET',
-    #     response_check=lambda response: response.status_code == 200,
-    #     dag=dag,
-    # )
-
-    # func_er_with_brute_force = SimpleHttpOperator(
-    #     task_id='func_er_with_brute_force',
-    #     http_conn_id='http_default',
-    #     endpoint='func_er_with_brute_force',
-    #     method='GET',
-    #     response_check=lambda response: response.status_code == 200,
-    #     dag=dag,
-    # )
-
-    func_calculate_quality_measures = SimpleHttpOperator(
-        task_id='func_calculate_quality_measures',
-        http_conn_id='http_default',
-        endpoint='local_func_calculate_quality_measures',
-        method='GET',
-        response_check=lambda response: response.status_code == 200,
-        dag=dag,
+    func_test = PythonOperator(
+        task_id='run_test_task',
+        python_callable=func_test
     )
 
+    fnc_calculate_quality_measures = PythonOperator(
+        task_id='run_calculate_quality_measures_task',
+        python_callable=f_calculate_quality_measures,  # Function to execute
+    )
 
-
-    # func_er_clustering = SimpleHttpOperator(
-    #     task_id='func_er_clustering',
-    #     http_conn_id='http_default',
-    #     endpoint='local_func_er_clustering',
-    #     method='GET',
-    #     response_check=lambda response: response.status_code == 200,
-    #     dag=dag,
-    # )
-
-    func_calculate_quality_measures
+    func_test >> fnc_calculate_quality_measures  # This executes the task
